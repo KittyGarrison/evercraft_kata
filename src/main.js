5e45336e3cf4eae161ebe156d3c588116f68725f
Katrina (Kitty) Garrison <katrina.garrison@gm.com>
2018-08-21 11:08:49 -0400

    initial commit

---
+++ src/main.js
@@ -0,0 +1,81 @@
+module.exports = {Character};
+const abilities = require('./abilities') ;
+
+
+// constructor function
+function Character(name) {
+  this.name = name || 'Fool';
+  this.getName = getName;
+  this.setName = setName;
+
+  this.alignment = 'good';
+  this.getAlignment = getAlignment;
+  this.setAlignment = setAlignment;
+
+  this.armorClass = 10;
+  this.getArmorClass = getArmorClass;
+
+  this.hitPoints = 5;
+  this.getHP = getHP;
+  this.reduseHitPoints = reduseHitPoints;
+  this.isDead = isDead;
+
+  this.hit = hit;
+  this.ability = abilities.abilities[0];
+};
+
+// static method on Character
+Character.create = function create(name) {
+  return new Character(name);
+};
+
+function getName(){
+  return this.name;
+  console.log(this.ability);
+}
+
+function setName(name){
+  this.name = name;
+}
+
+function getAlignment(){
+  return this.alignment;
+}
+
+function setAlignment(alignment){
+  alignment = alignment.toLowerCase();
+  const validAlignments = ['good','neutral','evil'];
+  if (validAlignments.includes(alignment)) {
+    this.alignment = alignment;
+  }else {
+    throw Error('invalid alignment')
+  };
+};
+
+function getArmorClass(){
+  return this.armorClass;
+};
+
+function getHP(){
+  return this.hitPoints;
+};
+
+function reduseHitPoints(damage){
+  this.hitPoints = this.hitPoints - damage
+};
+
+function isDead() {
+  return (this.hitPoints <= 0)
+};
+
+function hit(opponent, roll, damage){
+  if (roll === 20) {
+    damage = damage*2
+  }
+  if (opponent.getArmorClass() <= roll) {
+    opponent.reduseHitPoints(damage);
+    return true
+  }else {
+    return false
+  }
+};
